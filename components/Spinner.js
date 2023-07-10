5cf5b62c4dc83fc2cc1b2f9c9b14fd9e659520c8
DEBASMITROY2002 <ddruk2018@gmail.com>
2023-07-10 10:43:48 +0530

    iuiui

---
+++ components/Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SpinnerDotted } from 'spinners-react';
+
+const Spinner = () => {
+  return (
+    <div>
+      <SpinnerDotted size={50} thickness={158} speed={111} color="rgba(57, 142, 172, 1)" />
+    </div>
+  )
+}
+
+export default Spinner
