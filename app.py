a1054e72fc530ebf002889659c679795aa7d5bdb
Debasmit Roy <60804078+DEBASMITROY2002@users.noreply.github.com>
2024-09-10 10:43:41 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,228 +1,338 @@
-from common_imports import *
-from generic_engines import *
-from pydantic_schemas import *
-from agent_utils import *
-from langgraph_agent import *
-from prod_review_utils import *
-from MongoUtils import utils, userutils
-import hashlib
+import os,time
+from flask import Flask, request,jsonify
+from werkzeug.utils import secure_filename
+import json
+from flask_cors import CORS
+from dotenv import load_dotenv
+from flask_pymongo import PyMongo
+from query_proc_utils import *
+import networkx as nx
+from kg_maker_runner_utils import *
+from azure.storage.blob import BlobServiceClient
+import certifi
+from pypdf import PdfReader
 
-def hashStringToInt(inputString: str) -> int:
-    # Create a SHA-256 hash of the input string
-    hashObject = hashlib.sha256(inputString.encode())
-    # Convert the hash to a hexadecimal string
-    hexDig = hashObject.hexdigest()
-    # Convert the hexadecimal string to an integer
-    return int(hexDig, 16)
+load_dotenv()
 
- 
-app = flask.Flask(__name__)
+ALLOWED_EXTENSIONS = set({ 'pdf'})
+
+
+app = Flask(__name__)
+app.config['MONGO_URI'] = os.getenv('MONGO_URI')
+mongo = PyMongo(app,tlsCAFile=certifi.where())
+
 CORS(app)
 
+# To create a global Qdrant DB client
 
-sessions = {
-    10: {
-        "agent": None,
-        "cart": [], # List of product ids
-        "cart_budget":10000.,
-        "user_id": "user_1"
-    }
-}
-
-@app.route("/api/v1/user/insert", methods=["POST"])
-def insertUser():
-    user = request.json
-    userutils.insert(user)
-    return jsonify({"status": "success"}), 200
-
-@app.route("/api/v1/user/fetch", methods=["POST"])
-def getUser():
-    id = request.json["id"]
-    user = userutils.fetchById(id)
-    return jsonify(user), 200
-
-@app.route("/api/v1/products", methods=["GET"])
-def getProducts():
-    products = utils.fetchAll()
-    return jsonify(products), 200
+vector_store_wrapper = VectorStoreWrapper("qdrant_db")
+pdf_dict_path=os.path.join("./","pdf_dict_new.json")
 
 
-@app.route("/api/v1/products", methods=["POST"])
-def getProductById():
-    
-    id = request.json["id"]
-    
-    product = utils.fetchById(id)
-    return jsonify(product), 200
-
-@app.route("/api/v1/products/paginated", methods=["POST"])
-def get_paginated_products():
-    try:
-        # Assuming fetchPaginated is a function that retrieves products based on pagination
-        page_size = int(request.json["page_size"])
-        page_number = int(request.json["page_number"])
+def check_pdf_dict():
+    if  os.path.exists(pdf_dict_path):
         
-        products = utils.fetchPaginated(page_size, page_number)
-        return jsonify(products), 200
-    except ValueError as e:
-        # Handle the case where conversion to integer fails or other errors occur
-        return jsonify({"error": str(e)}), 400
-    except Exception as e:
-        # Handle unexpected errors
-        return jsonify({"error": "An unexpected error occurred: " + str(e)}), 500
+        with open(pdf_dict_path,'r') as f:
+            data=json.load(f)
 
-@app.route("/start-session", methods=["POST"])
-def start_session():
-    data = request.json
-    thread_id =  hashStringToInt(data["session_id"]) # Must be integer
-    sessions[thread_id] = {
-        "agent": MyAgent(thread_id),
-        "cart": [],
-        "cart_budget":10000.,
-        "user_id": "user_1"
-    }
-    return jsonify({"status": "success", "thread_id": thread_id})
-
-@app.route("/chat", methods=["POST"])
-def continue_flow():
-    try:
-        data = request.json
-        thread_id = hashStringToInt(data["session_id"])
-        agent = sessions[thread_id]["agent"]
-
-        if agent is None:
-            return jsonify({"status": "error", "message": "no agent found"})
-
-        snap = agent.get_recent_state_snap()
-
-        if len(snap.keys()) == 1:
-            messsages = []
-            if "age" in data and "gender" in data:
-                messsages.append(HumanMessage(f"Hi I am {data['age']} years old. My gender is {data['age']}). Initially, take this information into account while suggesting products."))
-            # Start the conversation
-            snap = agent.continue_flow({
-                "cur_state": "Requirement Phase",
-                "requirements": {},
-                "recently_added": [],
-                "mode":"primary",
-                "user_id": sessions[thread_id]["user_id"],
-                "new_place_req":{},
-                "messages": messsages,
-                "budget":10000.0,
-                "first_level_chosen":[]
-            })
-
-            print("Started with messages",snap["messages"])
-        else:
-            snap = agent.resume_with_user_input(data["user_input"])
-            sessions[thread_id]["cart_budget"] = snap["budget"]
+        keys=data.keys()
+        if "node1" in keys and "relation" in keys and "children" in keys:
+            return True
+    
+    with open(pdf_dict_path,'w') as f:
+        json.dump({
+            "node1": "All rules and prohibitions",
+            "relation": "points to",
+            "children":[]
+        },f,indent=4)
         
-        recently_added = snap["recently_added"]
+    return True
+
+
+
+
+check_pdf_dict()
+
+# Global object for QueryProcessing
+
+query_proc_obj = QueryProcessing(kg_path_dict_path=pdf_dict_path,vector_store_wrapper=vector_store_wrapper,store_new=False)
+
+
+# This function is to upload the rule book to Azure Blob Storage
+
+def upload_pdf_to_blob_storage(pdf_file_path, connection_string, container_name):
+    # Create a BlobServiceClient object which will be used to create a container client
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+
+    # Create a ContainerClient
+    container_client = blob_service_client.get_container_client(container_name)
+
+    # Create a BlobClient
+    blob_client = container_client.get_blob_client(os.path.basename(pdf_file_path))
+
+    # Upload the PDF file to the blob
+    with open(pdf_file_path, "rb") as data:
+        blob_client.upload_blob(data)
+
+    print(f"Uploaded {pdf_file_path} to Azure Blob Storage container {container_name}")
+
+
+def handle_pdf_processing(pdf_path,pdf_title,pdf_dict_path):
+    pdf_dict = kg_maker_run(
+            pdf_path = pdf_path, # Path to the pdf file
+            pdf_title = pdf_title, # Title of the pdf User wants to give
+            api_key_name_workers = ["OPENAI_API_KEY"],
+            init_dict_path = pdf_dict_path, # This is the path where the existing dict is stored
+            reset_neo4j = True,
+            store_at_neo4j = True
+        )
+
+    # Save
+    with open(pdf_dict_path,"w") as f:
+        json.dump(pdf_dict,f,indent=4)
+    print("json updated!!")
+
+    # Store the pdf_dict in COSMOS DB
+
+    mongo.db.rule_kg.insert_one(pdf_dict)
+
+    global query_proc_obj 
+    query_proc_obj = QueryProcessing(kg_path_dict_path=pdf_dict_path,vector_store_wrapper=vector_store_wrapper,store_new=True)
+
+
+
+@app.route('/upload', methods=['POST'])
+def fileUpload():
+    """
+    This function is to upload the rule book to the server
+
+    Returns:
+        response: str: Response message to the frontend
+
+    """
+    target = os.path.join('./ruleBooks/', 'test')
+    if not os.path.isdir(target):
+        os.mkdir(target)
+    file,file_name = request.files['file'],request.form['name']
+    
+
+    if file and file.filename.lower().endswith(tuple(ALLOWED_EXTENSIONS)):
+        filename = secure_filename(file.filename)
+        destination = os.path.join(target, filename)
+        file.save(destination)
+
+        reader=PdfReader(destination)
+
+        if len(reader.pages)>50:
+            response = jsonify({"error": "File Too Large. Please upload a PDF file with less than 50 pages."})
+             
+            return response
+
+        response = "Rule Book is uploaded successfully but it will be processed in the background. Please wait..."
+
+
+        pdf_path=destination
+        pdf_title=file_name
+        
+
+        # Upload the PDF to Azure Blob Storage if it doesn't already exist
+
         try:
-            text_content = agent.get_last_message().content
-            text_content = make_more_interactive_response(text_content)
-            return jsonify({
-                "status": "success",
-                "cur_state":snap['cur_state'], 
-                "text_content": text_content, 
-                "product_list": recently_added,
-                "budget":sessions[thread_id]["cart_budget"],
-                "new_place_req":snap["new_place_req"]
-                })
-        except:
-            snap = agent.get_recent_state_snap()
-            requirements = snap.get("requirements", {})
-            # Making agent None
-            sessions[thread_id]["agent"] = None
-            text_content = "No messages to show ! Have a good day !"
-            return jsonify({"status": "success",
-                            "cur_state":"completed",
-                            "text_content":f"No messages to show ! Have a good day !", 
-                            "product_list": [],
-                            "budget":sessions[thread_id]["cart_budget"],
-                            "new_place_req":snap["new_place_req"]
-                            })
-                            
-        
-    except Exception as e:
-        print(e)
-        return jsonify({"status": "error", "message": str(e)})
-        
+            upload_pdf_to_blob_storage(pdf_path, os.getenv('AZURE_STORAGE_CONNECTION_STRING'), os.getenv('AZURE_STORAGE_CONTAINER_NAME'))
+        except Exception as e:
+            print('*********************ALREADY EXISTS IN BLOB STORAGE*********************')
 
-@app.route("/add-to-cart", methods=["POST"])
-def add_to_cart():
-    data = request.json
-    thread_id = 10
+        pdf_processing_thread=threading.Thread(target=handle_pdf_processing,args=(pdf_path,pdf_title,pdf_dict_path))
+        pdf_processing_thread.start()
+        return response
+    else:
+        return "Invalid file format. Please upload a PDF file."
 
-    proudct_ids = data["product_ids"]
-    prices = data["prices"] # default [0]
+@app.route('/query',methods=['POST'])
+def getMsg():
+    """
+    This function is to process the query from the frontend
+    Insert the query in the COSMOS DB
 
-    for price in prices:
-        sessions[thread_id]["cart_budget"] -= price
-    sessions[thread_id]["cart"] += proudct_ids
-    return jsonify({"status": "success","budget":sessions[thread_id]["cart_budget"]})
+    Returns:
+        response: dict: Response message to the frontend which includes the violations and the risk category of the query along the response
+    """
+    # get the msg which the frontend sends and return response
+    query = request.get_json()
+    print(query) 
+   
     
+    start=time.time()
 
-@app.route("/get-cart", methods=["GET"])
-def get_cart():
-    thread_id = 10
-    cart = sessions[thread_id]["cart"]
-    return jsonify({"status": "success", "cart": cart})
+    # process the query through the query processing pipeline
+    response=query_proc_obj.process_query(query["query"])
 
-@app.route("/force-stop", methods=["POST"])
-def force_stop():
-    thread_id = 10
-    sessions[thread_id]["agent"] = None
-    return jsonify({"status": "success"})
-
-@app.route("/fetch-product", methods=["POST"])
-def fetch_product():
-    product_id = request.json["product_id"]
-    MONGO_HOSTED_URL = os.getenv("MONGO_HOSTED_URL")
-    total_url = f"{MONGO_HOSTED_URL}/api/v1/products/{product_id}"
-    print(total_url)
-    response = requests.get(total_url)
-    return jsonify(response.json())
-
-
-@app.route("/get-product-review-scores", methods=["POST"])
-def get_product_review_scores():
-    data = request.json
-    product_id = data["product_id"]  # 0513ab340eced913dce82594bd118ff0
-    first_level_cat = data["first_level_cat"] # tools_hardware , clothing
+    end=time.time()
+    # to append the time stamp . elapsed time and length of the query in a csv file
+    with open("latency.csv","a") as file:
+        file.write(f"{query['date_time']},{end-start},{len(query['query'])}\n")
     
-    if product_id not in ["0513ab340eced913dce82594bd118ff0"]:
-        return jsonify({"status": "error", "message": "Product not found"})
+    print(response)
+
+    query.update(response)
+
+    # Store the query in COSMOS DB
+
+    mongo.db.query.insert_one(query)
+
+    return jsonify(response)
+
+
+
+# This function is to get the Knowledge graph in the form of json from the backend and return it to the frontend
+
+@app.route('/getJson',methods=['GET'])
+def getJson():
+    try:
+        with open(pdf_dict_path,'r') as file:
+            data=json.load(file)
+        print("json fetched!!")
+        return jsonify(data)
+    except:
+        return jsonify({
+            "node1": "All rules and prohibitions",
+            "relation": "points to",
+            "children":[]
+        })
+
+
+# Fetch the knowledge graph data from json and give it to the graph maker function to create the graph 
+
+def getData():
+    try :
+        with open(pdf_dict_path,'r') as file:
+            data=json.load(file)
+        return data
+    except:
+        return {
+            "node1": "All rules and prohibitions",
+            "relation": "points to",
+            "children":[]
+        }
+
+
+# Add nodes and edges to the graph
+
+def add_nodes_and_edges(graph, data, parent_node=None, parent_relation=None):
+    for main_parent_node in data.get("children"):
+        main_parent=main_parent_node.get("node1")
+        main_parent_relation=main_parent_node.get("relation")
+        if main_parent is not None:
+            graph.add_node(main_parent,label=main_parent)
+            graph.add_edge(parent_node, main_parent,label=parent_relation)
+
+            for child_node in main_parent_node.get("children"):
+                child=child_node.get("node1")
+                child_relation=child_node.get("relation")
+                if child is not None:
+                    graph.add_node(child,label=child)
+                    graph.add_edge(main_parent, child,label=main_parent_relation)
+
+                    for grandchild_node in child_node.get("children"):
+                        grandchild=grandchild_node.get("node1")
+                        grandchild_relation=grandchild_node.get("relation")
+                        if grandchild is not None:
+                            graph.add_node(grandchild,label=grandchild)
+                            graph.add_edge(child, grandchild,label=child_relation)
+
+                            for great_grandchild_node in grandchild_node.get("children"):
+                                great_grandchild=great_grandchild_node.get("node1")
+                                great_grandchild_relation=great_grandchild_node.get("relation")
+                                great_grandchild_node2=great_grandchild_node.get("node2")
+                                if great_grandchild is not None:
+                                    graph.add_node(great_grandchild,label=great_grandchild,risk=great_grandchild_node.get("risk"))
+                                    graph.add_edge(grandchild, great_grandchild,label=grandchild_relation)
+
+                                    graph.add_node(great_grandchild_node2,label=great_grandchild_node2,risk=great_grandchild_node.get("risk"))
+                                    graph.add_edge(great_grandchild, great_grandchild_node2,label=great_grandchild_relation)
+
+
+
+
+@app.route('/getGraph',methods=['GET','POST'])
+def getGraph():
+    """
+    GET route: This function is to get the Knowledge graph in the form of json from the backend and return nodes and links of the graph to the frontend
+
+    POST route: This function is to update the Knowledge graph in the form of json from the frontend and return nodes and links of the updated graph to the frontend
+
+    Returns:
+        nodes: list: List of nodes in the graph
+        links: list: List of links in the graph
+    """
+
+    if request.method=='POST':
+        data=request.get_json()
+        # print(data)
+
+
+        with open(pdf_dict_path,'w') as file:
+            json.dump(data["data"],file,indent=4)
+        print("json updated!!")
+
+        check_pdf_dict() # check if the pdf_dict is in the correct format
+
+
+        # Store the new data in COSMOS DB
+        mongo.db.rule_kg.insert_one(data["data"])
+
+        global query_proc_obj
+        query_proc_obj = QueryProcessing(kg_path_dict_path=pdf_dict_path,vector_store_wrapper=vector_store_wrapper,store_new=True)
+
+   
+    data=getData()
+    graph = nx.DiGraph()
+   
     
+    root_node=data.get("node1")
+    root_node_relation=data.get("relation")
+    graph.add_node(root_node,label=root_node)
+    add_nodes_and_edges(graph, data,root_node,root_node_relation)
+
+    nodes = []
+    links = []
+
+    # Iterate over nodes and add their attributes to the nodes list
+    for node, attrs in graph.nodes(data=True):
+        node_info = {'id': node}
+        node_info.update(attrs)
+        nodes.append(node_info)
+
+
+    # Iterate over edges and add their attributes to the edges list
+    for source, target, attrs in graph.edges(data=True):
+        edge_info = {'source': source, 'target': target}
+        edge_info.update(attrs)
+        links.append(edge_info)
+
+    return jsonify(nodes,links)
+
+
+@app.route('/analytics',methods=['GET'])
+def getAnalytics():
+    """
+    This function is to get the analytics of the queries from the COSMOS DB and return it to the frontend
+
+    Returns:
+        data: list: List of queries from the COSMOS DB
+    """
+    _data=list(mongo.db.query.find())
     
-    monthly_averages,last_three_months_avg = calculate_review_details(
-        product_id, 
-        first_level_cat, 
-        reviews_map_path = "../stored-result-git/user-reviews-fake.json",
-        use_stored=True, 
-        store_path = '../stored-result-git/product-reviews-parmas.json')
-    
-    result = {
-        "monthly_averages": monthly_averages,
-        "last_three_months_avg": last_three_months_avg,
-        "product_id": product_id
-    }
-    return jsonify(result)
+    data=[]
+    for d in _data:
+        d.pop('_id',None)
+        data.append(d)
+    return jsonify(data)
+
+
+
 
 
 if __name__ == "__main__":
-    load_dotenv()
-    app.run(host="0.0.0.0", port=8080, debug=False)
-
-
-
-
-
-
-
-
-
-
-
-
-
+    app.secret_key = os.urandom(24)
+    app.run(debug=False,threaded=True,port=8000,host='0.0.0.0')
