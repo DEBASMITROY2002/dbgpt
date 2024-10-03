72531878efc554b1c92e9956d0eed9f457feb9bd
DEBASMITROY2002 <ddruk2018@gmail.com>
2024-10-03 14:36:44 +0530

    flant5 0

--- git-push.sh
+++ git-push.sh
@@ -2,12 +2,19 @@
 # Check if there is a parameter
 if [ -z "$1" ]
   then
+    echo "No branch mentioned"
+    exit 1
+fi
+
+if [ -z "$2" ]
+  then
     echo "No commit message supplied"
     exit 1
 fi
+
 echo "Add ..."
 git add .
 echo "Commit ..."
-git commit -m "$1"
+git commit -m "$2"
 echo "Push ..."
-git push
+git push origin $1
