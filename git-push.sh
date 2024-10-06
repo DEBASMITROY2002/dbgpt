332a4cbb46d337f234b17afd6dedc35844df5673
DEBASMITROY2002 <ddruk2018@gmail.com>
2024-10-06 17:25:25 +0530

    some updates

--- git-push.sh
+++ git-push.sh
@@ -1,6 +1,6 @@
 # message: Update as parameter
 # Check if there is a parameter
-if [ -z "$1" ]
+if [ -z "$2" ]
   then
     echo "No commit message supplied"
     exit 1
@@ -8,6 +8,6 @@ fi
 echo "Add ..."
 git add .
 echo "Commit ..."
-git commit -m "$1"
+git commit -m "$2"
 echo "Push ..."
-git push
+git push origin $1
